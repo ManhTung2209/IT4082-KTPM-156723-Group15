65b7c08948fd2caba9b530e1aa3526327e735332
thanh <thanh20042018@gmail.com>
2025-05-30 19:59:16 +0700

    Update bang status

--- Contributions/models.py
+++ Contributions/models.py
@@ -19,7 +19,24 @@ class Contribution(models.Model):
     is_reconciled = models.BooleanField(default=False)
 
     class Meta:
-        unique_together = ('household', 'code')  # Đảm bảo cặp household và code là duy nhất
+        unique_together = ('household', 'code')
 
     def __str__(self):
         return f"{self.household} - {self.amount} - {self.payment_date}"
+
+class ContributionStatus(models.Model):
+    household = models.ForeignKey(Household, on_delete=models.CASCADE, related_name='contribution_statuses')
+    code = models.ForeignKey(
+        Collection,
+        on_delete=models.CASCADE,
+        related_name='status_by_code',
+        to_field='code'
+    )
+    status = models.CharField(max_length=10, choices=[('Đã nộp', 'ĐÃ NỘP'), ('Chưa nộp', 'CHƯA NỘP')], default='CHƯA NỘP')
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('household', 'code')
+
+    def __str__(self):
+        return f"{self.household} - {self.code} - {self.status}"
